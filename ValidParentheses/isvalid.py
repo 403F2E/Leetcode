4079601f043a6364fd5a39232029acb52c4ec05b
BUSHIWAY <99516593+BUSHIWAY@users.noreply.github.com>
2025-03-13 23:47:26 +0000

    feat: Valid Parentheses (20)

---
+++ ValidParentheses/isvalid.py
@@ -0,0 +1,29 @@
+
+'''
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+ 1.Open brackets must be closed by the same type of brackets.
+ 2.Open brackets must be closed in the correct order.
+ 3.Every close bracket has a corresponding open bracket of the same type.
+'''
+
+def isValid(s: str) -> bool:
+    op, cl = "({[", ")}]"
+    if s[0] in cl:
+        return False
+
+    # The stack
+    stack: list = []
+    for i in range(len(s)):
+        if s[i] in op:
+            stack.append(op.index(s[i]))
+        elif s[i] in cl:
+            if not len(stack) or s[i] != cl[stack[-1]]:
+                return False
+            stack.pop()
+    
+    if not len(stack):
+        return True
+    return False
