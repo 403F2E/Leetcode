b05f548c0231b51658675540b093d48ee6a57fc5
BUSHIWAY <99516593+BUSHIWAY@users.noreply.github.com>
2025-04-13 21:25:25 +0100

    fix: fixing the Valid Palindrome probleme (125)

--- Palindrome/Palindrome.py
+++ Palindrome/Palindrome.py
@@ -1,10 +1,3 @@
 
-def isPalindrome(x: int):
-    y: str = str(x)
-    if x == y[::-1]:
-        return True
-    else :
-        return False
-
-x = int(input("enter an integr : "))
-print(isPalindrome(x))
+def isPalindrome(x: str):
+    return x == x[::-1]:
