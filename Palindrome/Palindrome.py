786e011240feff4732b5f5686c20be84fac5ec0c
BUSHIWAY <99516593+BUSHIWAY@users.noreply.github.com>
2025-04-13 21:47:27 +0100

    fix: fixing the Valid Palindrome probleme (125)

--- Palindrome/Palindrome.py
+++ Palindrome/Palindrome.py
@@ -1,3 +1,11 @@
 
-def isPalindrome(x: str):
-    return x == x[::-1]:
+'''
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+'''
+def isPalindrome(self, s: str) -> bool:
+    import re
+    s = s.lower()
+    s = re.sub("[\s_\W]", "", s)
+    return s == s[::-1]
