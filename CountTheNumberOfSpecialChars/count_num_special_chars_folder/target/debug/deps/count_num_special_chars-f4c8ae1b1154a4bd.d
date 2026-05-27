aabf377549b3bddf91bacbfb3a4443a3ba1f8f16
403F2E <shadowouss1@gmail.com>
2026-05-27 19:03:14 +0100

    fix: removing the CountNum... cargo folder

--- CountTheNumberOfSpecialChars/count_num_special_chars_folder/target/debug/deps/count_num_special_chars-f4c8ae1b1154a4bd.d
+++
@@ -1,5 +0,0 @@
-/home/puppeteer/dev/proj/Leetcode/CountTheNumberOfSpecialChars/count_num_special_chars/target/debug/deps/count_num_special_chars-f4c8ae1b1154a4bd.d: src/main.rs
-
-/home/puppeteer/dev/proj/Leetcode/CountTheNumberOfSpecialChars/count_num_special_chars/target/debug/deps/libcount_num_special_chars-f4c8ae1b1154a4bd.rmeta: src/main.rs
-
-src/main.rs:
